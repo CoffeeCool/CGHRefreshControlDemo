7da0d7f394ff8d6ebc9b8e149e4442b9683f2181
Coffee Cai <1136687@qq.com>
2016-03-07 09:49:41 +0800

    Initial Commit

---
+++ CGHRefreshControlDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CGHRefreshControlDemo
+//
+//  Created by caigehui on 16/3/7.
+//  Copyright © 2016年 caigehui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
